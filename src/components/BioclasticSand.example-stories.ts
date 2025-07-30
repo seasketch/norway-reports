ab26b38eee4f3aa14f147385185b5382ee509b53
Abby Vath Meyer <avmey@ucsb.edu>
2025-07-30 22:29:30 +0000

    Simplify

--- src/components/BioclasticSand.example-stories.ts
+++
@@ -1,9 +0,0 @@
-import { defineGpStories } from "@seasketch/geoprocessing/storybook";
-
-// Register to generate stories for each example sketch and its gp function smoke test output
-export const storyConfig = defineGpStories({
-  componentName: "BioclasticSand",
-  /** Relative path to React component from this config file */
-  componentPath: "./BioclasticSand.tsx",
-  title: "Project/Components/BioclasticSand",
-});
--- src/components/Eelgrass.example-stories.ts
+++
@@ -1,9 +0,0 @@
-import { defineGpStories } from "@seasketch/geoprocessing/storybook";
-
-// Register to generate stories for each example sketch and its gp function smoke test output
-export const storyConfig = defineGpStories({
-  componentName: "Eelgrass",
-  /** Relative path to React component from this config file */
-  componentPath: "./Eelgrass.tsx",
-  title: "Project/Components/Eelgrass",
-});
